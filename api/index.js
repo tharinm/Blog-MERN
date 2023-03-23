4a3aa9d1f8b9ea927872eb2c7b6069c7d31671be
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-23 15:50:25 +0530

    MONGODB connected

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res, next) => {
-   // console.log('This is main URL')
-})
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res, next) => {
+   console.log('This is main URL')
+});
 
 app.listen(8080, () => {
-    console.log('Backend is running..!')
-})
+  console.log("Backend is running..!");
+});
