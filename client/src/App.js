c340cd79e2e505294d0468525aeefb7346903ce3
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-29 18:10:31 +0530

    creted setting component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 //import Home from './pages/home/Home';
 import TopBar from './components/topbar/TopBar';
+import Setting from './pages/settings/Setting';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <TopBar />
-      <Write/>
+      {/* <Write/> */}
+      <Setting/>
     </div>
   );
 }
