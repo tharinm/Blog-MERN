29bf660e088e609cb87f921242e5c7d18032afe3
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-03 02:06:42 +0530

    create context provider

---
+++ client/src/context/Action.js
@@ -0,0 +1,13 @@
+export const LoginStart = (userCredential) => ({
+  type: "LOGIN_START"
+});
+
+
+export const LoginSuccess = (user) => ({
+    type: "LOGIN_SUCCESS",
+    payload:user,
+})
+
+export const LoginFailure = () => ({
+    type:"LOGIN_FAIL"
+})
