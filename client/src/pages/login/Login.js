a2ad5681f3ca13ab75e6d481f57cc746d4bdadbc
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-03 11:14:18 +0530

    create login with contextapi

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import './login.css'
+import React, { useContext } from "react";
+import "./login.css";
+import { useRef } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 export default function Login() {
-    return (
-      <div className="login">
-        <span className="loginTitle"></span>
-        <form className="loginForm">
-          <label>Email</label>
-          <input
-            className="loginInput"
-            type="text"
-            placeholder="Enter your email..."
-          />
-          <label>Password</label>
-          <input
-            className="loginInput"
-            type="password"
-            placeholder="Enter your password..."
-          />
-          <button className="loginButton">Login</button>
-        </form>
-        <button className="loginRegisterButton">Register</button>
-      </div>
-    );
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user,dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password:passwordRef.current.value,
+      });
+       dispatch({ type: "LOGIN_SUCCESS",payload:res.data });
+    } catch (err) {
+       dispatch({ type: "LOGIN_FAILURE"});
+    }
+  };
+
+  console.log(isFetching);
+  console.log(user)
+  return (
+    <div className="login">
+      <span className="loginTitle"></span>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          className="loginInput"
+          type="text"
+          placeholder="Enter your username..."
+          ref={userRef}
+        />
+        <label>Password</label>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password..."
+          ref={passwordRef}
+        />
+        <button className="loginButton" type="submit">
+          Login
+        </button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </div>
+  );
 }
